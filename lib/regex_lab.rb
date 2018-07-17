f8cb416a06cfbb311419d59a7213780f4832a009
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 05:49:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,22 +1,17 @@
 def starts_with_a_vowel?(word)
 word.match(/\b[aeiou]/i) ? true : false
-
 end
-
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan = (/un\w+ing/i)
-  Regexp.union(reg)
-  text.to_a
-
+  text.scan(/un\w+ing/i)
 end
 
-def words_five_letters_long(word)
+def words_five_letters_long(text)
 text.scan = /\b[a-z]{5}\b/i
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(word)
-text.match(/^[A-Z].+\$/) ? true : false
+text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 
