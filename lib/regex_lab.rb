ff66469f2f97e677bdf9d73fd067145749b359fa
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 05:14:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+Regexp.new("/\A[^aeiou]/") !=nil
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
+reg ("^un", "ing")
+Regexp.union(reg)
+text.to_a
 
 end
 
-def words_five_letters_long(text)
+def words_five_letters_long(word)
+
+"extreme briny crepe parking snaps grouping snafu round dog be fork spoon"
+matches = (/\[(.*?)\](.*)/.scan('[ruby] regex'))
+matches.to_a
 
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
+def first_word_capitalized_and_ends_with_punctuation?(word)
+!!Regexp.union("^[A-Z]", ".")
 end
 
-def valid_phone_number?(phone)
 
-end
+# def valid_phone_number?(phone)
+# Regexp.new("~/\b^([0-9]{10})$\b/")
+# return (phone.match(phone))?
+# end
+
+#regex union multiple
