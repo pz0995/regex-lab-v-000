070fbd1a935ad19ce5faf11294ca7525eb261e4e
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 06:02:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ def words_five_letters_long(text)
 text.scan(/\b[a-z]{5}\b/i)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(word)
+def first_word_capitalized_and_ends_with_punctuation?(text)
 text.match(/^[A-Z].+\.$/) ? true : false
 end
 
